b7a8fc807ff7a690956745f3351c250f1b1f640d
apl <antoineplddev@gmail.com>
2021-03-18 17:14:02 +0100

    Add CircularSlider

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '../components/Link';
+import CircularSlider from '@fseehawer/react-circular-slider';
 
 export default function Index() {
     return (
@@ -14,6 +15,21 @@ export default function Index() {
                 <Link href="/about" color="secondary">
                     Go to the about page
                 </Link>
+            </Box>
+            <Box my={4}>
+                <CircularSlider
+                    max={180}
+                    label="Angular rotation"
+                    labelColor="#005a58"
+                    knobColor="#005a58"
+                    progressColorFrom="#00bfbd"
+                    progressColorTo="#009c9a"
+                    progressSize={24}
+                    trackColor="#eeeeee"
+                    trackSize={24}
+                    dataIndex={10}
+                    onChange={value => { console.log(value); }}
+                />
             </Box>
         </Container>
     );
